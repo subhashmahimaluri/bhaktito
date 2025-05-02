3293ada4134ab2a1339d23963d9c37a10fb7e9a3
Subhash Mahimaluri <subbumahimaluri@gmail.com>
2025-05-02 23:27:34 +0530

    Skip Tests

--- src/components/__tests__/PanchangamTable.test.tsx
+++ src/components/__tests__/PanchangamTable.test.tsx
@@ -95,7 +95,7 @@ describe('PanchangamTable', () => {
     });
   });
 
-  it.skip('renders "N/A" for Nakshatra when name_en_IN is null', async () => {
+  it('renders "N/A" for Nakshatra when name_en_IN is null', async () => {
     vi.mocked(MhahPanchang).mockImplementationOnce(() => {
       return {
         calendar: vi.fn().mockReturnValue({
